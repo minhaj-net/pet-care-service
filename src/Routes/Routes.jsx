77a79fce426ec2d6384a3025456f7d45e55a08a0
minhaj-net <infoglb.net@gmail.com>
2025-10-21 20:41:27 +0600

    Set up Pet Care Service project structure with basic configuration , initial assets and npm package

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path:"/home",
+    element:<div>This is home pageds</div>
+  }
+]);
